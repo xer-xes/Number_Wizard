5f7b22f419ef7de99bdf55f6f87347d757e855bf
xer-xes <31579450+xer-xes@users.noreply.github.com>
2019-04-30 16:59:54 +0530

    Final
    Competed Project

---
+++ Assets/Number_Wizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Number_Wizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    [SerializeField] TextMeshProUGUI Guess_Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        Guess_Text.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+}
